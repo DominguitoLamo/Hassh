5695acf47dd75b758eae1d5d794d1799957425c1
chonlam <1685020647@qq.com>
2023-11-09 13:46:37 +0800

    feat: add group dao model

---
+++ src/internal/model/groupTasksModel/grouptasksmodel.go
@@ -0,0 +1,29 @@
+package group_task
+
+import "github.com/zeromicro/go-zero/core/stores/sqlx"
+
+var _ GroupTasksModel = (*customGroupTasksModel)(nil)
+
+type (
+	// GroupTasksModel is an interface to be customized, add more methods here,
+	// and implement the added methods in customGroupTasksModel.
+	GroupTasksModel interface {
+		groupTasksModel
+		withSession(session sqlx.Session) GroupTasksModel
+	}
+
+	customGroupTasksModel struct {
+		*defaultGroupTasksModel
+	}
+)
+
+// NewGroupTasksModel returns a model for the database table.
+func NewGroupTasksModel(conn sqlx.SqlConn) GroupTasksModel {
+	return &customGroupTasksModel{
+		defaultGroupTasksModel: newGroupTasksModel(conn),
+	}
+}
+
+func (m *customGroupTasksModel) withSession(session sqlx.Session) GroupTasksModel {
+	return NewGroupTasksModel(sqlx.NewSqlConnFromSession(session))
+}
